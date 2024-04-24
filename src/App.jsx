f4e0b608f8d372c92566c0ce48cae1b4137f5eb7
Aish Mishra <aishlunatic001@gmail.com>
2024-04-24 22:41:13 +0530

    framing sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 
 import './index.css'
 
-function App() {
+const App = ()=>(
+  <main className='relative'>
+    nav
+
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      Hero
+    </section>
+
+    <section className='padding'>
+      PopularProducts
+    </section>
+    <section className='padding'>
+      SuperQuality
+    </section>
+    <section className='padding'>
+      Services
+    </section>
+    <section className='padding'>
+      SpecialOffers
+    </section>
 
 
-  return (
-    <>
-      <h1 className='text-blue-500 text-center'> Hare Krishna </h1>
-    </>
-  )
-}
-
+    <section className='bg-pale-blue padding'>
+      CustomerReviews 
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      Subscribe
+    </section>
+    <section className='bg-black padding-x padding-t pb-8'>
+      Footer
+    </section>
+  </main>
+)
 export default App
